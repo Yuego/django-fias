80335ad90b623a6853c833960c30e732bab6851d
Yuego [Lenovo] <root@proscript.ru>
2013-09-11 09:57:20 +0000

    Восстановил работоспособность импортера:
     - заменил pysimplesoap на suds
     - удалённые записи теперь также импортируются, но не импортируются
       изменённые
     - немного оптимизировал импортер - текущая дата хранится в переменной,
       а не рассчитывается на в каждой итерации

--- fias/config.py
+++ fias/config.py
@@ -4,9 +4,10 @@ from __future__ import unicode_literals, absolute_import
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-_FIAS_TABLES_DEFAULT = ['socrbase', 'normdoc', 'addrobj', 'house']
+_FIAS_TABLES_DEFAULT = ('socrbase', 'normdoc', 'addrobj', 'house')
+FIAS_DELETED_TABLES = ('addrobj', 'house', 'houseint', 'normdoc')
 
-FIAS_TABLES = getattr(settings, 'FIAS_TABLES', _FIAS_TABLES_DEFAULT)
+FIAS_TABLES = list(getattr(settings, 'FIAS_TABLES', _FIAS_TABLES_DEFAULT))
 
 FIAS_DATABASE_ALIAS = getattr(settings, 'FIAS_DATABASE_ALIAS', 'fias')
 
