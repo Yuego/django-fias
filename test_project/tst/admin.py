cdff0793a5617555b06b9a425e5f0f1e48485674
Yuego [Lenovo] <root@proscript.ru>
2013-09-12 15:53:11 +0000

    Дополнил README и добавил ещё пару моделей в тестовое приложение

--- test_project/tst/admin.py
+++ test_project/tst/admin.py
@@ -1,10 +1,13 @@
 
 from django.contrib import admin
 
-from tst.models import Item
+from tst.models import *
 
 class ItemAdmin(admin.ModelAdmin):
     class Media:
         js = ['//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.js']
 
 admin.site.register(Item, ItemAdmin)
+admin.site.register(CachedAddress, ItemAdmin)
+admin.site.register(CachedAddressWithArea, ItemAdmin)
+
--- test_project/tst/models.py
+++ test_project/tst/models.py
@@ -3,9 +3,17 @@ from django.db import models
 # Create your models here.
 
 from fias.fields import AddressField
+from fias.models import FIASAddress, FIASAddressWithArea
 
 class Item(models.Model):
     
-    title = models.CharField('Title', max_length=100)
+    title = models.CharField('title', max_length=100)
 
     location = AddressField()
+
+class CachedAddress(FIASAddress):
+    pass
+
+
+class CachedAddressWithArea(FIASAddressWithArea):
+    pass
