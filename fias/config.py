131d868483b9d803b5c84e63d00b2cfd8a9ab156
Yuego [Lenovo] <root@proscript.ru>
2013-09-13 15:02:23 +0000

    Отделил обязательные к импорту таблицы.

--- fias/config.py
+++ fias/config.py
@@ -4,10 +4,12 @@ from __future__ import unicode_literals, absolute_import
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-_FIAS_TABLES_DEFAULT = ('socrbase', 'normdoc', 'addrobj', 'landmark', 'houseint', 'house')
-FIAS_DELETED_TABLES = ('addrobj', 'house', 'houseint', 'normdoc')
+_DEFAULT = ('landmark', 'houseint', 'house')
 
-FIAS_TABLES = list(getattr(settings, 'FIAS_TABLES', _FIAS_TABLES_DEFAULT))
+FIAS_TABLES = ['socrbase', 'normdoc', 'addrobj']
+FIAS_TABLES.extend([x for x in list(getattr(settings, 'FIAS_TABLES', _DEFAULT)) if x in _DEFAULT])
+
+FIAS_DELETED_TABLES = ('addrobj', 'house', 'houseint', 'normdoc')
 
 FIAS_DATABASE_ALIAS = getattr(settings, 'FIAS_DATABASE_ALIAS', 'fias')
 
