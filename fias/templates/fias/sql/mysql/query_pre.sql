3ad63ab69ef555033e8f5a813522cd3e8ee81474
Yuego [Lenovo] <root@proscript.ru>
2013-11-27 19:40:18 +0400

    Фикс на случай дублирующихся записей в SocrBase

--- fias/templates/fias/sql/mysql/query_pre.sql
+++ fias/templates/fias/sql/mysql/query_pre.sql
@@ -1,14 +1,14 @@
 insert into fias_addrobjindex (aoguid, aolevel, scname, fullname)
 (SELECT
 	ao.aoguid, ao.aolevel,
-	(SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao.shortname AND sn.level=ao.aolevel) as scname,
+	(SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao.shortname AND sn.level=ao.aolevel LIMIT 1) as scname,
 	concat(ao.shortname, " ", ao.formalname) as fullname
 	FROM fias_addrobj as ao
 	where ao.aolevel=1)
 union
 (select
 	ao2.aoguid, ao2.aolevel,
-	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel)) as scname,
+	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel LIMIT 1)) as scname,
 	concat(ao1.shortname, " ", ao1.formalname, ", ", ao2.shortname, " ", ao2.formalname) as fullname
 	from
 		fias_addrobj as ao1
@@ -19,7 +19,7 @@ union
 union
 (select
 	ao3.aoguid, ao3.aolevel,
-	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel)) as scname,
+	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel LIMIT 1)) as scname,
 	concat(ao1.shortname, " ", ao1.formalname, ", ", ao2.shortname, " ", ao2.formalname, ", ", ao3.shortname, " ", ao3.formalname) as fullname
 	from
 		fias_addrobj as ao1
@@ -33,7 +33,7 @@ union
 union
 (select
 	ao4.aoguid, ao4.aolevel,
-	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao4.shortname AND sn.level=ao4.aolevel)) as scname,
+	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao4.shortname AND sn.level=ao4.aolevel LIMIT 1)) as scname,
 	concat(ao1.shortname, " ", ao1.formalname, ", ", ao2.shortname, " ", ao2.formalname, ", ", ao3.shortname, " ", ao3.formalname, ", ", ao4.shortname, " ", ao4.formalname) as fullname
 	from
 		fias_addrobj as ao1
@@ -50,7 +50,7 @@ union
 union
 (select
 	ao5.aoguid, ao5.aolevel,
-	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao4.shortname AND sn.level=ao4.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao5.shortname AND sn.level=ao5.aolevel)) as scname,
+	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao4.shortname AND sn.level=ao4.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao5.shortname AND sn.level=ao5.aolevel LIMIT 1)) as scname,
 	concat(ao1.shortname, " ", ao1.formalname, ", ", ao2.shortname, " ", ao2.formalname, ", ", ao3.shortname, " ", ao3.formalname, ", ", ao4.shortname, " ", ao4.formalname, ", ", ao5.shortname, " ", ao5.formalname) as fullname
 	from
 		fias_addrobj as ao1
@@ -70,7 +70,7 @@ union
 union
 (select
 	ao6.aoguid, ao6.aolevel,
-	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao4.shortname AND sn.level=ao4.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao5.shortname AND sn.level=ao5.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao6.shortname AND sn.level=ao6.aolevel)) as scname,
+	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao4.shortname AND sn.level=ao4.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao5.shortname AND sn.level=ao5.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao6.shortname AND sn.level=ao6.aolevel LIMIT 1)) as scname,
 	concat(ao1.shortname, " ", ao1.formalname, ", ", ao2.shortname, " ", ao2.formalname, ", ", ao3.shortname, " ", ao3.formalname, ", ", ao4.shortname, " ", ao4.formalname, ", ", ao5.shortname, " ", ao5.formalname, ", ", ao6.shortname, " ", ao6.formalname) as fullname
 	from
 		fias_addrobj as ao1
@@ -93,7 +93,7 @@ union
 union
 (select
 	ao6.aoguid, ao6.aolevel,
-	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao4.shortname AND sn.level=ao4.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao5.shortname AND sn.level=ao5.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao6.shortname AND sn.level=ao6.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao7.shortname AND sn.level=ao7.aolevel)) as scname,
+	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao4.shortname AND sn.level=ao4.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao5.shortname AND sn.level=ao5.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao6.shortname AND sn.level=ao6.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao7.shortname AND sn.level=ao7.aolevel LIMIT 1)) as scname,
 	concat(ao1.shortname, " ", ao1.formalname, ", ", ao2.shortname, " ", ao2.formalname, ", ", ao3.shortname, " ", ao3.formalname, ", ", ao4.shortname, " ", ao4.formalname, ", ", ao5.shortname, " ", ao5.formalname, ", ", ao6.shortname, " ", ao6.formalname) as fullname
 	from
 		fias_addrobj as ao1
@@ -119,7 +119,7 @@ union
 union
 (select
 	ao8.aoguid, ao8.aolevel,
-	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao4.shortname AND sn.level=ao4.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao5.shortname AND sn.level=ao5.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao6.shortname AND sn.level=ao6.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao7.shortname AND sn.level=ao7.aolevel), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao8.shortname AND sn.level=ao8.aolevel)) as scname,
+	concat((SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao1.shortname AND sn.level=ao1.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao2.shortname AND sn.level=ao2.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao3.shortname AND sn.level=ao3.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao4.shortname AND sn.level=ao4.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao5.shortname AND sn.level=ao5.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao6.shortname AND sn.level=ao6.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao7.shortname AND sn.level=ao7.aolevel LIMIT 1), ", ", (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao8.shortname AND sn.level=ao8.aolevel LIMIT 1)) as scname,
 	concat(ao1.shortname, " ", ao1.formalname, ", ", ao2.shortname, " ", ao2.formalname, ", ", ao3.shortname, " ", ao3.formalname, ", ", ao4.shortname, " ", ao4.formalname, ", ", ao5.shortname, " ", ao5.formalname, ", ", ao6.shortname, " ", ao6.formalname, ", ", ao7.shortname, " ", ao7.formalname, ", ", ao8.shortname, " ", ao8.formalname) as fullname
 	from
 		fias_addrobj as ao1
--- fias/templates/fias/sql/pgsql/query.sql
+++ fias/templates/fias/sql/pgsql/query.sql
@@ -1,12 +1,12 @@
 WITH RECURSIVE PATH (docid, aoguid, aolevel, scname, fullname) AS (
   SELECT NEXTVAL('fias_addrobj_id_seq') AS docid, ao.aoguid, ao.aolevel,
-    (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao.shortname AND sn.level=ao.aolevel)::TEXT AS scname,
+    (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=ao.shortname AND sn.level=ao.aolevel LIMIT 1)::TEXT AS scname,
     ao.shortname || ' ' || formalname AS fullname
   FROM fias_addrobj AS ao
   WHERE aolevel = 1 AND livestatus = TRUE
   UNION
   SELECT NEXTVAL('fias_addrobj_id_seq') AS docid, child.aoguid, child.aolevel,
-    PATH.scname::TEXT || ', ' || (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=child.shortname AND sn.level=child.aolevel) AS scname,
+    PATH.scname::TEXT || ', ' || (SELECT sn.socrname FROM fias_socrbase AS sn WHERE sn.scname=child.shortname AND sn.level=child.aolevel LIMIT 1) AS scname,
     PATH.fullname || ', ' || child.shortname || ' ' || child.formalname AS fullname
   FROM fias_addrobj AS child, PATH
   WHERE child.parentguid = PATH.aoguid AND livestatus = TRUE
