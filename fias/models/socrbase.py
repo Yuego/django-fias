2aa98489bd0e784175cc737d9aa9bea1a3f944e9
Malyshev Alex <malyshevalex@gmail.com>
2015-01-27 21:20:30 +0500

    add @python_2_unicode_compatible

--- fias/models/socrbase.py
+++ fias/models/socrbase.py
@@ -1,5 +1,6 @@
 #coding: utf-8
 from __future__ import unicode_literals, absolute_import
+from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
@@ -7,6 +8,7 @@ from django.utils.translation import ugettext_lazy as _
 __all__ = ['SocrBase']
 
 
+@python_2_unicode_compatible
 class SocrBase(models.Model):
 
     class Meta:
@@ -22,9 +24,6 @@ class SocrBase(models.Model):
     kod_t_st = models.PositiveIntegerField(primary_key=True)
 
     item_weight = models.PositiveSmallIntegerField(default=64)
-
-    def __unicode__(self):
-        return self.socrname
 
     def __str__(self):
         return self.socrname
