0185fa25e8a124fb449befa81aa18ca9b2bac4c9
Vlasov Artem [Lenovo] <root@proscript.ru>
2016-09-13 17:12:50 +0400

    Обновил Loader

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -137,6 +137,18 @@ FIAS_TABLES = [
     'stead',
     'houseint',
 ]
+
+FIAS_TABLE_ROW_FILTERS = {
+    'addrobj': (
+        'fias.importer.filters.example_filter_yaroslavl_region',
+    ),
+    'house': (
+        'fias.importer.filters.example_filter_yaroslavl_region',
+    ),
+    'room': (
+        'fias.importer.filters.example_filter_yaroslavl_region',
+    ),
+}
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
