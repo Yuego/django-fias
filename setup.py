19dc83e6dfdb3905419356fb86045e3f2c317c0d
Yuego [Home] <root@proscript.ru>
2017-01-03 22:35:22 +0400

    Зависимость для Sphinx. Из-за бага 123 версия 1.3.8 неработоспособна с этим движком

--- setup.py
+++ setup.py
@@ -103,6 +103,7 @@ setup(
     install_requires=[
         'django >= 1.8, < 1.10',
         'django_select2>=5.3.0',
+        'mysqlclient != 1.3.8',
         # 'zeep>=0.8.0',
         'rarfile',
         'six',
